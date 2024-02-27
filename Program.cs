631dc27a20eec1dc7bc629bf19da30a59fe7dae4
rjtechdev <yo-rez@outlook.com>
2024-02-27 14:34:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace rst_reg_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
